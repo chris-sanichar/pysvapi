89608bc951583e73485096a1d60fd4a167823a65
Chris <csanichar@sandvine.com>
2017-06-22 18:48:58 -0400

    changed url

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup,find_packages
 setup(name='pysvapi',
       version='0.2',
       description='Sandvine Python API',
-      url='http://github.com/mfmarche/pysvapi',
+      url='http://github.com/chris-sanichar/pysvapi',
       author='Sandvine',
-      author_email='mmarchetti@sandvine.com',
+      author_email='csanichar@sandvine.com',
       license='Apache2',
       packages=find_packages(),
       install_requires=[
